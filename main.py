062b435306cc5bdd8b6a1d2400c7cb1ef9557cb7
dleinwan <dani.l.wander@gmail.com>
2022-11-11 10:00:14 -0500

    added classes

---
+++ main.py
@@ -0,0 +1,30 @@
+
+class Organism:
+    def __init__(self) -> None:
+        self.fitness = 0
+        #randomize genes?
+        self.genes = []
+        pass
+
+class Population:
+    
+
+    def __init__(self) -> None:
+        self.popSize = 10
+        self.organisms = []
+        for i in range(0,self.popSize):
+            self.organisms.append(Organism())
+        pass
+
+# START
+# Generate the initial population
+# Compute fitness
+# REPEAT
+#     Selection
+#     Crossover
+#     Mutation
+#     Compute fitness
+# UNTIL population has converged
+# STOP
+
+
