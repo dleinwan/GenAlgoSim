cdf52c542249241ca64d51a3611f6d9e8a7a8860
dleinwan <dani.l.wander@gmail.com>
2022-12-04 22:08:56 -0500

    basic gen algo example

--- main.py
+++ main.py
@@ -10,21 +10,31 @@
 #                                (this way we will not have to deal with computing convergence)
 # STOP
 
+import random
+
 class Organism:
-    def __init__(self) -> None:
+    genes = []
+    fitness = 0
+
+    def __init__(self):
         #TODO: randomize fitness
-        self.fitness = 0
+        self.fitness = random.randrange(1,100)
         #TODO: randomize genes
-        self.genes = []
+        #
+        #example
+        randomNum = random.randrange(1,100)
+        self.genes = [randomNum]
+
         pass
 
 # Population is made up of organisms
 class Population:
-    def __init__(self) -> None:
-        #TODO: decide on population size
-        self.popSize = 10
-        self.organisms = []
-        for organism in self.organisms:
+    organisms = []
+    #TODO: decide on population size
+    size = 200 #make sure divisible by 4
+
+    def __init__(self):
+        for i in range(self.size):
             self.organisms.append(Organism())
         pass
 
@@ -37,15 +47,19 @@ def main():
     currGeneration = Population()
 
     #TODO: compute fitness
-    computeFitness(1, currGeneration)
+    computeFitness(1, currGeneration, currGeneration)
 
-    for genNum in range(numGens-1):
+    printFitness(1, currGeneration)
+
+    for genNum in range(1, numGens + 1):
         # STORE NEW DATA HERE
         nextGeneration = Population()
         print(genNum)
 
         #TODO: selection (kill off one half)
         selection(genNum, currGeneration, nextGeneration)
+
+        printFitness(genNum, currGeneration)
 
         #TODO: crossover (reproduce)
         crossover(genNum, currGeneration, nextGeneration)
@@ -65,21 +79,48 @@ def main():
     pass
 
 
-def computeFitness(genNum, currGen):
-    #TODO: compute fitness for first generation
-    print("Computing Fitness for generation" + genNum)
-
 def computeFitness(genNum, currGen, nextGen):
-    print("Computing Fitness for generation" + genNum)
+    print("Computing Fitness for generation " + str(genNum))
+    #
+    #example
+    for organism in currGen.organisms:
+        organism.fitness = organism.genes[0] % random.randrange(1,50) #random number to mod by 
+        #print("fit: " + str(organism.fitness))
+        # note: if your fitness calculation is the exact same every time, convergence will happen very quickly
+    pass
+    
 
 def selection(genNum, currGen, nextGen):
-    print("Selecting best performers" + genNum)
+    print("Selecting best performers " + str(genNum))
+    #
+    #example
+    currGen.organisms.sort(key=lambda x: x.fitness, reverse=False) #sort population list by fitness
+    pass
 
-def crossover(genNum, pocurrGenp, nextGen):
-    print("Crossing over" + genNum)
+def crossover(genNum, currGen, nextGen):
+    print("Crossing over " + str(genNum))
+    #
+    #example: the top 75-100 percentile and 50-75 percentile make two children each
+    orgPerP = currGen.size / 4  #organisms per 25th percentile
+    for parOne in range(int((currGen.size * 3/4)) - 1, currGen.size):
+        # if size=200, parOne (first parent) from 149-199
+        parTwo = parOne - 50  #parent two index
+        #if size=200, replace lowest 100 with children of parents
+        childOne = parOne - 199
+        childTwo = parOne - 198
+        currGen.organisms[childOne].genes[0] = currGen.organisms[parOne].genes[0] + currGen.organisms[parTwo].genes[0]
+        currGen.organisms[childTwo].genes[0] = currGen.organisms[parOne].genes[0] - currGen.organisms[parTwo].genes[0]
+    pass
 
-def mutation(genNum, pocurrGenp, nextGen):
-    print("Mutating" + genNum)
+
+def mutation(genNum, currGen, nextGen):
+    print("Mutating " + str(genNum))
+    pass
+
+
+def printFitness(genNum, currGen):
+    for organism in currGen.organisms:
+        print(organism.fitness)
 
 
 if __name__ == "__main__":
